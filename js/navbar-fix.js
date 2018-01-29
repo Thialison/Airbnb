a87af0a9a53259b0251b8794dd5a9d283bb7cfa0
Thialison <thialison.souza@hotmail.com>
2018-01-29 17:10:26 -0200

    Scroll da página pela tag a

--- js/navbar-fix.js
+++ js/navbar-fix.js
@@ -1,8 +1,16 @@
 $('#navbar').on('show.bs.collapse', function(){
-    $('.topCasaFina-banner').css('transform', 'translate(-50%, 30%)');
+    $('.topCasaFina-banner').css('transform', 'translate(-50%, 0%)');
 });
 
 
 $('#navbar').on('hide.bs.collapse', function(){
     $('.topCasaFina-banner').css('transform', 'translate(-50%, -50%)');
 });;
+
+var $doc = $('html, body');
+$('a').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
